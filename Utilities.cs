77fcf155642d7c4d440e5c5270f509b22c4087d8
ZachCarpenter296 <54647059+ZachCarpenter296@users.noreply.github.com>
2019-09-03 11:11:47 -0600

    Add files via upload

---
+++ Utilities.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Utilities
+{
+    public static string ProcessText(string textIn)
+    {
+            try
+            {
+                float test = float.Parse(textIn);
+                textIn = "Float";
+
+            }
+            catch
+            {
+                textIn = "String";
+            }
+        return textIn;
+    }
+}
+
+
+//return the word "string" if the user enters a string
+//return the word "number" if the user enters a number (including 1.2 decimal)
+
